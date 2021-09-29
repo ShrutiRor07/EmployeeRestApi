ccdfe43b2550b30eeccfddb22440187be3708a58
shrutiror <shruti.ror@nagarro.com>
2021-09-29 22:42:49 +0530

    Uploaded EmployeeRESTApi all files

---
+++ src/main/java/com/nagarro/advancedjava/assignment5/ServletInitializer.java
@@ -0,0 +1,13 @@
+package com.nagarro.advancedjava.assignment5;
+
+import org.springframework.boot.builder.SpringApplicationBuilder;
+import org.springframework.boot.web.servlet.support.SpringBootServletInitializer;
+
+public class ServletInitializer extends SpringBootServletInitializer {
+
+	@Override
+	protected SpringApplicationBuilder configure(SpringApplicationBuilder application) {
+		return application.sources(EmployeeRestApiApplication.class);
+	}
+
+}
---
+++ src/test/java/com/nagarro/advancedjava/assignment5/EmployeeRESTApi/EmployeeRestApiApplicationTests.java
@@ -0,0 +1,13 @@
+package com.nagarro.advancedjava.assignment5.EmployeeRESTApi;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class EmployeeRestApiApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
